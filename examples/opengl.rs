3989b97a6af947bde39fd02919b981758e16d26b
Udrea Dan <udreadanmihai@gmail.com>
2022-02-06 21:29:45 +0200

    Update

---
+++ examples/opengl.rs
@@ -0,0 +1,167 @@
+use std::{fs::OpenOptions, io::Read, time::Duration, collections::HashMap};
+use objld::*;
+use rayon::{iter::{ParallelIterator}};
+use std::hash::Hash;
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct ParsedVertex{
+    pos: (F32Wrapper, F32Wrapper, F32Wrapper),
+    tex: (F32Wrapper, F32Wrapper),
+    norm: (F32Wrapper, F32Wrapper, F32Wrapper)
+}
+
+#[derive(Debug, Clone, Copy)]
+struct F32Wrapper{
+    inner: f32
+}
+impl F32Wrapper{
+    fn key(&self) -> u64{
+        let prec: u16 = 7;// digits in b10
+        let whole = self.inner as u64;
+        let frac = ((self.inner as f64 - whole as f64) * 10.0_f64.powi(prec.into())) as u64;
+        whole*10u64.pow(prec.into()) + frac
+    }
+}
+
+impl PartialEq for F32Wrapper{
+    fn eq(&self, other: &Self) -> bool {
+        self.key() == other.key()
+    }
+}
+impl Eq for F32Wrapper{}
+impl Hash for F32Wrapper{
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.key().hash(state)
+    }
+}
+
+impl From<f32> for F32Wrapper{
+    fn from(v: f32) -> Self {
+        Self{inner: v}
+    }
+}
+
+impl From<F32Wrapper> for f32{
+    fn from(v: F32Wrapper) -> Self{
+        v.inner
+    }
+}
+
+#[derive(Default)]
+struct RawData3D{
+    vertex_pos: Vec<(f32, f32, f32)>,
+    vertex_tex: Vec<(f32, f32)>,
+    vertex_norm: Vec<(f32, f32, f32)>,
+    vert_ind: Vec<objld::VertexIndeces<i32>>
+}
+
+fn to_raw_data3d(parsed_data: Vec<LineResult<f32, i32>>) -> RawData3D{
+    let mut r = RawData3D::default();
+    for line in parsed_data{
+        match line{
+            LineResult::VertDataLine(v) => match v{
+                VertexData::Coord3 { x, y, z } => {r.vertex_pos.push((x, y, z));},
+                VertexData::Normal { x, y, z } => {r.vertex_norm.push((x, y, z)); },
+                VertexData::TextureCoord2 { u, v } => {r.vertex_tex.push((u, v))},
+                _ => {},
+            },
+            LineResult::FaceLine(f) => match f{
+                Face::Face3 { v1, v2, v3 } => {r.vert_ind.push(v1); r.vert_ind.push(v2); r.vert_ind.push(v3);},
+                Face::Face4 { v1, v2, v3, v4 } => {r.vert_ind.push(v1); r.vert_ind.push(v2); r.vert_ind.push(v3);  r.vert_ind.push(v3); r.vert_ind.push(v4); r.vert_ind.push(v1); },
+            },
+            LineResult::NoData => {},
+            LineResult::Error(_e) => {} // Ignore unparsed data
+        }
+    }
+    r
+}
+
+#[derive(Default)]
+struct OpenGLData3D{
+    vertex_pos: Vec<(f32, f32, f32)>,
+    vertex_tex: Vec<(f32, f32)>,
+    vertex_norm: Vec<(f32, f32, f32)>,
+    indecies: Vec<u32>
+}
+
+fn to_opengl_data3d_dedup(raw_data: RawData3D) -> OpenGLData3D{
+    let mut o = OpenGLData3D::default();
+    let mut h: HashMap<ParsedVertex, u32> = HashMap::new();
+    h.reserve(raw_data.vertex_pos.len());
+    let mut curr_ind: u32 = 0;
+    for v in raw_data.vert_ind{
+        let coord_ind = v.coord_rindex;
+        let tex_ind = v.texcoord_rindex.unwrap();
+        let norm_ind = v.normal_rindex.unwrap();
+        let pos_tuple = raw_data.vertex_pos[if coord_ind < 0 { raw_data.vertex_pos.len() as i32 + coord_ind} else {coord_ind} as usize];
+        let tex_tuple = raw_data.vertex_tex[if tex_ind < 0 { raw_data.vertex_tex.len() as i32 + tex_ind} else {tex_ind} as usize];
+        let norm_tuple = raw_data.vertex_norm[if norm_ind < 0 { raw_data.vertex_norm.len() as i32 + norm_ind} else {norm_ind} as usize];
+        let pv = ParsedVertex{
+            pos: (pos_tuple.0.into(), pos_tuple.1.into(), pos_tuple.2.into()),
+            tex: (tex_tuple.0.into(), tex_tuple.1.into()),
+            norm: (norm_tuple.0.into(), norm_tuple.1.into(), norm_tuple.2.into())
+          };
+          if let Some(repeated_ind) = h.get(&pv){
+              o.indecies.push(*repeated_ind);
+          }else{
+              h.insert(pv, curr_ind);
+              o.indecies.push(curr_ind);
+              curr_ind += 1;
+          }
+    }
+    o.vertex_pos.resize(h.len(), (0.0, 0.0, 0.0));
+    o.vertex_tex.resize(h.len(), (0.0, 0.0));
+    o.vertex_norm.resize(h.len(), (0.0, 0.0, 0.0));
+    for (v, i) in h {
+        o.vertex_pos[i as usize] = (v.pos.0.into(), v.pos.1.into(), v.pos.2.into());
+        o.vertex_tex[i as usize] = (v.tex.0.into(), v.tex.1.into());
+        o.vertex_norm[i as usize] = (v.norm.0.into(), v.norm.1.into(), v.norm.2.into());
+    }
+    o
+}
+
+fn to_opengl_data3d_simple(raw_data: RawData3D) -> OpenGLData3D{
+    let mut o = OpenGLData3D::default();
+    o.vertex_pos.reserve(raw_data.vert_ind.len());
+    o.vertex_tex.reserve(raw_data.vert_ind.len());
+    o.vertex_norm.reserve(raw_data.vert_ind.len());
+    o.indecies.reserve(raw_data.vert_ind.len());
+    let mut curr_ind = 0;
+    for v in raw_data.vert_ind{
+        let coord_ind = v.coord_rindex;
+        let tex_ind = v.texcoord_rindex.unwrap();
+        let norm_ind = v.normal_rindex.unwrap();
+        let pos_tuple = raw_data.vertex_pos[if coord_ind < 0 { raw_data.vertex_pos.len() as i32 + coord_ind} else {coord_ind} as usize];
+        let tex_tuple = raw_data.vertex_tex[if tex_ind < 0 { raw_data.vertex_tex.len() as i32 + tex_ind} else {tex_ind} as usize];
+        let norm_tuple = raw_data.vertex_norm[if norm_ind < 0 { raw_data.vertex_norm.len() as i32 + norm_ind} else {norm_ind} as usize];
+        o.vertex_pos.push(pos_tuple);
+        o.vertex_tex.push(tex_tuple);
+        o.vertex_norm.push(norm_tuple);
+        o.indecies.push(curr_ind);
+        curr_ind += 1;
+    }
+    o
+}
+
+fn main(){
+    let t;
+    
+    let raw_data: RawData3D = {
+        let mut f = OpenOptions::new().read(true).write(false).open("rungholt.obj").expect("Opening .obj file!");
+        let mut buf = String::new();
+        f.read_to_string(&mut buf).expect("Reading .obj file!");
+        t = std::time::Instant::now();
+        let parsed = parse_file(&buf).collect();
+        println!("Prasing took: {}s!", Duration::as_secs_f32(&(std::time::Instant::now()-t)));
+        to_raw_data3d(parsed)
+    };
+    
+    let raw_no_verts = raw_data.vertex_pos.len();
+    println!("No. of vertices before: {}", raw_no_verts);
+    println!("Marker: {}s!", Duration::as_secs_f32(&(std::time::Instant::now()-t)));
+    let o = to_opengl_data3d_dedup(raw_data);
+    println!("Transforming to opengl data: {}s", Duration::as_secs_f32(&(std::time::Instant::now()-t)));
+    println!("No. of vertices after: {}", o.vertex_pos.len()); // Assumes o.vertex_pos.len() == o.vertex_tex.len() == o.vertex_norm.len()
+    println!("Increase: {}% more than raw", ((o.vertex_pos.len() as f64 - raw_no_verts as f64)/raw_no_verts as f64) * 100.0);
+    println!("Final parsing time: {}s", Duration::as_secs_f32(&(std::time::Instant::now()-t)));
+}
---
+++ src/lib.rs
@@ -0,0 +1,250 @@
+use std::str::FromStr;
+
+use nom::{IResult, branch::alt, bytes::complete::tag, character::complete::{digit0, digit1, one_of, space0, space1}, combinator::{eof, opt, rest}, sequence::tuple};
+use nom::combinator::{map, recognize};
+use nom::character::complete::char;
+use rayon::{iter::ParallelIterator, str::ParallelString};
+
+
+#[derive(Debug, PartialEq)]
+pub enum VertexData<T>{
+    Coord2{ x: T, y: T }, // Unofficial 
+    Coord3{ x: T, y: T, z: T },
+
+    // No support for w in Coords
+
+    Normal{x: T, y: T, z: T},
+
+    TextureCoord3{ u: T, v: T, w: T },
+    TextureCoord2{ u: T, v: T },
+    TextureCoord1{ u: T }
+}
+
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct VertexIndeces<I>{
+    pub coord_rindex: I,
+    pub texcoord_rindex: Option<I>,
+    pub normal_rindex: Option<I>,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Face<I>{
+    Face3{
+        v1: VertexIndeces<I>,
+        v2: VertexIndeces<I>,
+        v3: VertexIndeces<I>        
+    },
+
+    Face4{
+        v1: VertexIndeces<I>,
+        v2: VertexIndeces<I>,
+        v3: VertexIndeces<I>,
+        v4: VertexIndeces<I>
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_float1() {
+        let (_, res) = consume_num("-1234.1, hello!").unwrap();
+        assert_eq!(res, "-1234.1");
+    }
+
+    #[test]
+    fn test_float2() {
+        let (_, res) : (_, f32) = parse_float("2, hj!").unwrap();
+        assert_eq!(res, 2.0);
+    }
+
+    #[test]
+    fn test_vert(){
+        let (unconsumed, res) : (_, VertexData<f32>) = parse_coord2(" v 1.0 -2.0 # hi!").unwrap();
+        assert_eq!(res, VertexData::Coord2{x: 1.0, y: -2.0});
+        assert_eq!(unconsumed, " # hi!");
+    }
+
+    #[test]
+    fn test_texvert(){
+        let (_, res) : (_, LineResult<f32, u32>) = parse_line("vt 1.0 +2.0 3.0").unwrap();
+        if let LineResult::VertDataLine(res) = res{
+            assert_eq!(res, VertexData::TextureCoord3{u: 1.0, v: 2.0, w: 3.0});
+        }else{ panic!("Wrong line type!"); }
+    }
+
+    #[test]
+    fn test_vert_space(){
+        let (_, res) : (_, LineResult<f32, u32>) = parse_line("    v      -5.000000       5.000000       0.000000").unwrap();
+        if let LineResult::VertDataLine(res) = res{
+            assert_eq!(res, VertexData::Coord3{x: -5.0, y: 5.0, z: 0.0});
+        }else{ panic!("Wrong line type!"); }
+    }
+
+    #[test]
+    fn test_normal(){
+        let res : IResult<_, VertexData<f32>> = parse_normal(" vn 1.0 -2.0 # hi!");
+        assert_eq!(res, Err(nom::Err::Error(nom::error::Error::new("# hi!", nom::error::ErrorKind::Digit)))) 
+    }
+    #[test]
+    fn test_num1(){
+        let (_, res) : (_, i32) = parse_num("1").unwrap();
+        assert_eq!(res, 1i32);
+    }
+
+    #[test]
+    fn test_face1(){
+        use std::convert::TryInto;
+        let (_, res) : (_, LineResult<f32, u32>) = parse_line("    f 1/2/3 3//2 2/1/").unwrap();
+        if let LineResult::FaceLine(Face::Face3{v1, v2, v3}) = res{
+            assert_eq!(v1, VertexIndeces::<u32>{coord_rindex: 1.try_into().unwrap(), texcoord_rindex: Some(2.try_into().unwrap()), normal_rindex: Some(3.try_into().unwrap())});
+            assert_eq!(v2, VertexIndeces::<u32>{coord_rindex: 3.try_into().unwrap(), texcoord_rindex: None,    normal_rindex: Some(2.try_into().unwrap())});
+            assert_eq!(v3, VertexIndeces::<u32>{coord_rindex: 2.try_into().unwrap(), texcoord_rindex: Some(1.try_into().unwrap()), normal_rindex: None});
+        }else{ panic!("Wrong line type!"); }
+    }
+}
+
+// A line can either contain vertex info or face info as far as this parser is concerned
+#[derive(Debug)]
+pub enum LineResult<'a, T, I>{
+    VertDataLine(VertexData<T>),
+    FaceLine(Face<I>),
+    NoData,
+    Error(nom::Err<nom::error::Error<&'a str>>)
+}
+
+
+pub fn parse_file<'input, T, I>(input: &'input str) -> impl ParallelIterator<Item = LineResult<T, I>>
+where T: Send + FromStr + 'input, I: Send + FromStr + 'input{
+    input.par_split('\n')
+    .map(|line|
+        parse_line(line)
+        .map(|(_unconsumed, parsed)| parsed)
+        .unwrap_or_else(|e|{
+            LineResult::Error(e)
+        })
+    )
+}
+
+
+fn parse_line<T, I>(input: &str) -> IResult<&str, LineResult<T, I>>
+where T: FromStr, I: FromStr{
+    use LineResult::VertDataLine;
+    use LineResult::FaceLine;
+    use LineResult::NoData;
+    alt((
+        map(end_line, |_| NoData), // If the line doesn't contain anything just return None
+
+        // 1 float
+        map(tuple((parse_texcoord1, end_line)), |(v, _)| VertDataLine(v) ),
+
+        // 2 floats
+        map(tuple((parse_coord2, end_line)), |(v, _)| VertDataLine(v) ),
+        map(tuple((parse_texcoord2, end_line)), |(v, _)| VertDataLine(v) ),
+
+        // 3 floats
+        map(tuple((parse_coord3, end_line)), |(v, _)| VertDataLine(v) ),
+        map(tuple((parse_normal, end_line)), |(v, _)| VertDataLine(v) ),
+        map(tuple((parse_texcoord3, end_line)), |(v, _)| VertDataLine(v) ),
+
+        // 3 fields
+        map(tuple((parse_face3, end_line)), |(f, _)| FaceLine(f)),
+
+        // 4 fields
+        map(tuple((parse_face4, end_line)), |(f, _)| FaceLine(f)),
+    ))(input)
+
+}
+
+/// Primitive parsers
+/**********************************************************************************/
+#[inline]
+fn consume_num(input: &str) -> IResult<&str, &str>{ recognize(tuple( ( opt(one_of("+-")), digit1, opt(char('.')), digit0) ) )(input) }
+
+#[inline]
+fn parse_float<T>(input: &str) -> IResult<&str, T>
+where T: FromStr{
+    let (input, num) = consume_num(input)?;
+    let val: T = T::from_str(num).map_err(|_| nom::Err::Error(nom::error::Error::new(num, nom::error::ErrorKind::Float)))?;
+    return Ok((input, val));
+}
+
+#[inline]
+fn parse_num<T>(input: &str) -> IResult<&str, T>
+where T: FromStr{
+    let (input, num) = consume_num(input)?;
+    let val: T = str::parse(num).map_err(|_| nom::Err::Error(nom::error::Error::new(num, nom::error::ErrorKind::Digit)))?;
+    return Ok((input, val));
+}
+
+#[inline]
+fn end_line(input: &str) -> IResult<&str, &str>{
+    type Comment<'a> = &'a str;
+   fn consume_comment(input: &str) -> IResult<&str, Comment> { recognize(tuple((space0, char('#'), rest)))(input) }
+   recognize(tuple((  space0, opt(consume_comment), eof  )))(input)
+}
+/**********************************************************************************/
+
+// For 2d vertex coords
+fn parse_coord2<T>(input: &str) -> IResult<&str, VertexData<T>>
+where T: FromStr {
+    let (input, data) = tuple(( space0, tag("v"), space1, parse_float, space1, parse_float ))(input)?;
+    Ok((input, VertexData::Coord2{x: data.3, y: data.5}))
+}
+
+// For 3d vertex coords
+fn parse_coord3<T>(input: &str) -> IResult<&str, VertexData<T>>
+where T: FromStr {
+    let (input, data) = tuple(( space0, tag("v"), space1, parse_float, space1, parse_float, space1, parse_float ))(input)?;
+    Ok((input, VertexData::Coord3{x: data.3, y: data.5, z: data.7}))
+}
+
+// For 3d normals (no support for 2d normals)
+fn parse_normal<T>(input: &str) -> IResult<&str, VertexData<T>>
+where T: FromStr {
+    let (input, data) = tuple(( space0, tag("vn"), space1, parse_float, space1, parse_float, space1, parse_float ))(input)?;
+    Ok((input, VertexData::Normal{x: data.3, y: data.5, z: data.7}))
+}
+
+// For 1D textures
+fn parse_texcoord1<T>(input: &str) -> IResult<&str, VertexData<T>>
+where T: FromStr {
+    let (input, data) = tuple(( space0, tag("vt"), space1, parse_float ))(input)?;
+    Ok((input, VertexData::TextureCoord1{u: data.3}))
+}
+
+// For 2D textures
+fn parse_texcoord2<T>(input: &str) -> IResult<&str, VertexData<T>>
+where T: FromStr {
+    let (input, data) = tuple(( space0, tag("vt"), space1, parse_float, space1, parse_float ))(input)?;
+    Ok((input, VertexData::TextureCoord2{u: data.3, v: data.5}))
+}
+
+// For 3D textures
+fn parse_texcoord3<T>(input: &str) -> IResult<&str, VertexData<T>>
+where T: FromStr{
+    let (input, data) = tuple(( space0, tag("vt"), space1, parse_float, space1, parse_float, space1, parse_float ))(input)?;
+    Ok((input, VertexData::TextureCoord3{u: data.3, v: data.5, w: data.7}))
+}
+
+// For face3 and face4
+fn parse_face_vertex<I>(input: &str) -> IResult<&str, VertexIndeces<I>>
+where I: FromStr {
+    let (input, data) = tuple(( parse_num, char('/'), opt(parse_num), char('/'), opt(parse_num) ))(input)?; // NUM/OPT(NUM)/OPT(NUM)
+    Ok((input, VertexIndeces{  coord_rindex: data.0, texcoord_rindex: data.2, normal_rindex: data.4 }))
+}
+
+// For triangle faces
+fn parse_face3<I>(input: &str) -> IResult<&str, Face<I>>
+where I: FromStr {
+    let (input, data) = tuple(( space0, tag("f"), space1, parse_face_vertex, space1, parse_face_vertex, space1, parse_face_vertex ))(input)?;
+    Ok((input, Face::Face3{ v1: data.3, v2: data.5, v3: data.7 })) // Intentionally ignore data.9
+}
+
+// For square faces
+fn parse_face4<I>(input: &str) -> IResult<&str, Face<I>>
+where I: FromStr {
+    let (input, data) = tuple(( space0, tag("f"), space1, parse_face_vertex, space1, parse_face_vertex, space1, parse_face_vertex, space1, parse_face_vertex ))(input)?;
+    Ok((input, Face::Face4{ v1: data.3, v2: data.5, v3: data.7, v4: data.9 }))
+}
